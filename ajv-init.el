7d3768dccc98d0c81bf8759055ef271a7dc98923
Akshar Varma <aksharvarma@gmail.com>
2018-06-13 11:22:52 -0400

    Add shell-pop package into init

--- ajv-init.el
+++ ajv-init.el
@@ -44,6 +44,14 @@
   (ido-mode t)
   (setq ido-enable-flex-matching t))
 
+(use-package shell-pop
+  :bind (("C-M-1" . shell-pop))
+  :config
+  (setq shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+  (setq shell-pop-term-shell "/bin/bash")
+  ;; need to do this manually or not picked up by `shell-pop'
+  (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type))
+
 (use-package python
   ;; :disabled
   :mode ("\\.py\\'" . python-mode)
