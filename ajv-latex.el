6c58a979b66650c2c7df330037cc4ec31d8fe483
Akshar Varma <aksharvarma@gmail.com>
2019-03-30 18:16:08 -0400

    Add more AUCTex configs from jrvarma's repo

--- ajv-latex.el
+++ ajv-latex.el
@@ -5,4 +5,30 @@
       ;; To make AUCTeX read/update on changes to .bib files.
       TeX-parse-self nil ; Enable parse on load. [DISABLED]
       TeX-auto-save nil ; Enable parse on save. [DISABLED]
-      reftex-plug-into-AUCTeX t)
+      TeX-engine 'xetex
+      reftex-plug-into-AUCTeX t
+      reftex-ref-macro-prompt nil)
+
+(defun texcount ()
+  "Counts the number of words in the .tex file using the texcount command.
+Taken from jrv-auctex-config.el in the repo: https://github.com/jrvarma/dot-emacs/"
+  (interactive)
+  (let* ((this-file (buffer-file-name))
+	 (word-count
+          (with-output-to-string
+            (with-current-buffer standard-output
+              (call-process "texcount" nil t nil "-1" this-file)))))
+    (message word-count))
+  )
+
+;; The idea for the following also comes from jrvarma's dot-emacs repo.
+(setq TeX-view-program-selection
+      (quote
+       (((output-dvi has-no-display-manager)
+	 "dvi2tty")
+	((output-dvi style-pstricks)
+	 "xdg-open")
+	(output-dvi "xdvi")
+	(output-pdf "PDF Tools")
+	(output-html "xdg-open"))
+       ))
