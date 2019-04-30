4a38423a00afcce5dbced301b020105f764fcfc8
Akshar Varma <aksharvarma@gmail.com>
2019-04-29 20:53:16 -0400

    Enable copy-paste to other programs only if display-graphic-p is t

--- ajv-misc.el
+++ ajv-misc.el
@@ -1,17 +1,20 @@
 (provide 'ajv-misc)
 
-(setq ;; Copy-pasting to-from other programs
-      select-enable-clipboard t        ;copy/paste into other programs
-      interprogram-paste-function 'x-selection-value ;from other progams
-      next-line-add-newlines t
-      sentence-end-double-space nil ;sentences don't have 2 spaces after '.'
-      indent-tabs-mode nil     ;Don't use tabs for indenting
-      async-shell-command-buffer 'new-buffer         ;always use new buffer
-      enable-recursive-minibuffers 1
-      ;; max-specpdl-size 13000
-      find-file-visit-truename t    ;follow symlinks to true targets
-      vc-follow-symlinks t    ;follow symlinks that are in git repo
-      time-stamp-pattern nil)            ;use local variables for time-stamps
+(when (display-graphic-p)
+  ;; Copy-pasting to-from other programs when in display graphics
+  (setq  select-enable-clipboard t        ;copy/paste into other programs
+	 interprogram-paste-function 'x-selection-value) ;from other progams
+  )
+
+(setq  next-line-add-newlines t
+       sentence-end-double-space nil ;sentences don't have 2 spaces after '.'
+       indent-tabs-mode nil     ;Don't use tabs for indenting
+       async-shell-command-buffer 'new-buffer         ;always use new buffer
+       enable-recursive-minibuffers 1
+       ;; max-specpdl-size 13000
+       find-file-visit-truename t    ;follow symlinks to true targets
+       vc-follow-symlinks t    ;follow symlinks that are in git repo
+       time-stamp-pattern nil)            ;use local variables for time-stamps
 
 (setq custom-file (concat ajv/my-init-directory ajv/custom-file-name))
 (load custom-file)
