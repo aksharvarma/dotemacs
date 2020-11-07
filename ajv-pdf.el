946d323c72bd1de2610b00424db203e7b84be1d9
Akshar Varma <aksharvarma@gmail.com>
2020-11-07 11:45:15 -0500

    Cleanup useless lines from ajv-pdf

--- ajv-pdf.el
+++ ajv-pdf.el
@@ -30,25 +30,6 @@
   (force-mode-line-update 1)		;Needed to actually see the change
   )
 
-;; (defun ajv/pdf-tools/kill-pdf-buffer-and-delete-frame ()
-;;   "Kill the PDF buffer and then delete the frame."
-;;   (interactive)
-;;   ;; (kill-buffer (current-buffer))
-;;   (delete-frame)
-;;   ;; (let (pdf-buffer (current-buffer))
-;;   ;;   (delete-frame)
-;;   ;;   (when ((member pdf-buffer (buffer-list)))
-;;   ;;     (kill-buffer pdf-buffer)))
-;;   )
-
-;; (defun ajv/pdf-tools/advise-find-file-to-open-pdf-in-new-frame (returned-buffer)
-;;   "Put an advice after find-file so that all PDFs are opened in a new dedicated frame rather than in a window in same frame."
-;;   (when (with-current-buffer returned-buffer (derived-mode-p 'pdf-view-mode))
-;;     (execute-kbd-macro (read-kbd-macro "C-x z"))
-;;     (switch-to-buffer-other-frame returned-buffer t)
-;;     (revert-buffer t t t))
-;;   )
-
 (defun ajv/pdf-tools/launch-file (returned-buffer)
   "Put an advice after find-file so that all PDFs are opened as a separate emacsclient (xdg-open) call."
   (when (with-current-buffer returned-buffer (derived-mode-p 'pdf-view-mode))
@@ -56,4 +37,3 @@
     (let ((process-connection-type nil))
       (start-process "*launch*" nil "xdg-open" (buffer-file-name returned-buffer))))
   )
-;; (start-process-shell-command "pdf-test" "pdf-test-output" (concat "emacsclient -a '' -c " "/home/akshar/aksharfiles/Files/Books/Inheritance Series/1)Eragon.pdf"))
