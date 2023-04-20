4909cee21211e9d2bf7e571d2186bc12d3865a66
Akshar Varma <aksharvarma@gmail.com>
2023-04-20 13:06:52 -0400

    Add cleveref stuff for LaTeX

--- ajv-latex.el
+++ ajv-latex.el
@@ -19,3 +19,15 @@ Taken from jrv-auctex-config.el in the repo: https://github.com/jrvarma/dot-emac
   (if (eq reftex-cite-format 'natbib)
       (setq reftex-cite-format 'default)
     (setq reftex-cite-format 'natbib)))
+
+
+(defvar-local ajv/latex/use-cleveref nil
+  "A variable that is used by the `ajv/latex/reftex-reference-cleveref-wrapper'
+function to determine whether to use the cleveref package's reference system of the default")
+
+(defun ajv/latex/reftex-reference-cleveref-wrapper ()
+  "Automatically choose reftex's preferred reference format between \\cref and the default based on the file local variable `ajv/latex/use-cleveref' which defaults to nil. So please set that variable to true if you wish to use cleveref by default."
+  (interactive)
+  (if ajv/latex/use-cleveref
+      (call-interactively 'reftex-cleveref-cref)
+    (call-interactively 'reftex-reference)))
